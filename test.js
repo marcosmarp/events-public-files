7e736500e13c166ce4b0449bdcf3ffd379f81099
Marcos <marcosmartinezpalermo@gmail.com>
2022-07-15 13:22:45 -0300

    testing values catching

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-console.log("here from github");
+window.onload = () => {
+    const nameInput = document.querySelector(
+        '[placeholder="Nombre y apellido"]'
+    );
+
+    const submitButton = document.querySelector('[type="submit"]');
+
+    // when the user clicks the submit button
+    submitButton?.addEventListener('click', () => {
+        // @ts-ignore
+        if (!nameInput?.value) return;
+        // @ts-ignore
+        console.log(`Desde github: ${nameInput.value}`);
+    }
+};
