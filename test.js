d6de5b4b9f4c03728dc70c8680ccc0b3ac6d9e1e
Marcos <marcosmartinezpalermo@gmail.com>
2022-07-15 16:33:39 -0300

    logging form

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     const form = document.getElementById("form");
+    console.log(form);
     // on form submit, get console.log all values
     form?.addEventListener("submit", (e) => {
         console.log(e);
