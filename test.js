e6d13fe1842916cb6f272a409bf90cba7535da58
Marcos <marcosmartinezpalermo@gmail.com>
2022-07-15 16:30:41 -0300

    test catching form

--- test.js
+++ test.js
@@ -1,18 +1,8 @@
 window.onload = () => {
-    const nameInput = document.querySelector(
-        '[placeholder="Nombre y apellido"]'
-    );
-
-    const submitButton = document.getElementsByTagName("button");
-
-    console.log(nameInput);
-    console.log(submitButton);
-
-    // when the user clicks the submit button
-    submitButton[0].addEventListener("click", () => {
-        // @ts-ignore
-        if (!nameInput?.value) return;
-        // @ts-ignore
-        console.log(`Desde github: ${nameInput.value}`);
+    const form = document.getElementById("form");
+    // on form submit, get console.log all values
+    form?.addEventListener("submit", (e) => {
+        console.log(e);
+        console.log(form);
     });
 };
